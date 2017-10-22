7fd2218973b4c936e09f92b62975ad89d84681f1
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-10-21 22:02:44 -0400

    ch ch ch changes

--- launch_rest.js
+++ launch_rest.js
@@ -4,7 +4,11 @@ var util = require("util")
 module.exports.rest = function(userdata, userport){
 	return (function(data, port) {
 		http.createServer((req,res) => {
-			res.writeHead(200, {"content-type": "application/json"})
+			res.writeHead(200, {
+				"Content-type": "application/json",
+				'Content-Encoding': 'utf8',
+				'Content-Length': Buffer.byteLength(data)
+		})
 			res.end(util.inspect(data))
 		}).listen(port)
 	})(userdata, userport)
